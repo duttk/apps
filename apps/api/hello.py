07cfb6a696dd6f8dc74c7422df8caf29a9ec2eda
Kaushik Dutt <duttk92@gmail.com>
2020-04-25 03:14:52 -0700

    CORS

--- apps/api/hello.py
+++ apps/api/hello.py
@@ -5,6 +5,7 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
+    self.send_header('Access-Control-Allow-Origin', '*')
     self.end_headers()
     self.wfile.write('Hello from Python'.encode())
     return
